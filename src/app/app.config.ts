64752763d47b0a7fcd35934718d0c8f16cbf2504
André Kilchenmann <github@milchkannen.ch>
2019-04-30 13:06:45 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 export class AppConfig {
     // when did you start with the time-line?
-    public static YEAR: number = 2018;
-    public static MONTH: number = 6;
-    public static DAY: number = 16;
+    public static YEAR: number = 2019;
+    public static MONTH: number = 5;
+    public static DAY: number = 1;
 }
 
