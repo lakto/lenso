be64353d1c9a810d26eee77b67cbee8925d91d42
André Kilchenmann <github@milchkannen.ch>
2020-04-07 15:17:24 +0200

    Update image path

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,5 +3,7 @@ export class AppConfig {
     public static YEAR: number = 2019;
     public static MONTH: number = 5;
     public static DAY: number = 1;
+    // url to images
+    public static PATH: string = "http://media.lakto.org/365/";
 }
 
