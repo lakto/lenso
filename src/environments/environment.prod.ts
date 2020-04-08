53b4f3075be54d73a143b2c2bd5a543b21cf0a80
André Kilchenmann <github@milchkannen.ch>
2020-04-08 11:59:45 +0200

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  production: true,
-  start: {
-    year: 2019,
-    month: 5,
-    day: 1
-  },
-  end: {
-    year: 2019,
-    month: 5,
-    day: 31
-  },
-  imagePath: "http://media.lakto.org/365/"
+    production: true,
+    start: {
+        year: 2019,
+        month: 5,
+        day: 1
+    },
+    end: {
+        year: 2019,
+        month: 5,
+        day: 31
+    },
+    imagePath: "https://media.lakto.org/365/"
 };
