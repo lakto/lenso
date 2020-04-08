720fc10372e95c1e39d4e593080fc1d4dc2207f6
André Kilchenmann <github@milchkannen.ch>
2020-04-08 11:38:16 +0200

    Delete everything

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-export class AppConfig {
-    // when did you start with the time-line?
-    public static YEAR: number = 2019;
-    public static MONTH: number = 5;
-    public static DAY: number = 1;
-    // url to images
-    public static PATH: string = "http://media.lakto.org/365/";
-}
-
